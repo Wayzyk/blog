69209a25bd0e1284f73834b5e13f84bb3786cdd3
Wayzyk <romakorobko@gmail.com>
2017-10-10 00:14:32 +0300

    Add and update Users & comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
+  post 'users' => 'users#create'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009192254) do
+ActiveRecord::Schema.define(version: 20171009211154) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20171009192254) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
