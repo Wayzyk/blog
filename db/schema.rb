41f66d7a01230efd7d8191057ec1f67a970fc3d9
Wayzyk <romakorobko@gmail.com>
2017-10-10 00:46:55 +0300

    Update comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009211154) do
+ActiveRecord::Schema.define(version: 20171009211937) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20171009211154) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
